e5fc84a23546bfb014b135e06f2cedd0cecfdef6
Alireza <alirezax69prom@gmail.com>
2023-06-16 14:53:27 +0330

    change password length limit.
    - because in `config/index.json` we can config password length,
    in joi schema i using this configs to validating password.

--- src/modules/auth/schema/index.ts
+++ src/modules/auth/schema/index.ts
@@ -1,15 +1,20 @@
 import joi from 'joi';
+import config from '../../../config/index.json';
 import { InputServiceType } from '../interface';
 
 export const SignupSchema = joi.object<InputServiceType.SignupInput>({
   name: joi.string().lowercase().required(),
   family: joi.string().lowercase().required(),
   email: joi.string().email(),
-  password: joi.string().required().min(5)
+  password: joi
+    .string()
+    .required()
+    .min(config.User.MIN_PASSWORD_LENGTH)
+    .max(config.User.MAX_PASSWORD_LENGTH)
 });
 
 export const SigninSchema = joi.object<InputServiceType.SigninInput>({
   ip: joi.string(),
   email: joi.string().trim().email({}).required(),
-  password: joi.string().required().min(5)
+  password: joi.string().required()
 });
